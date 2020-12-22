f2f53957ee640fb4ef958d51fdca2e7547399f60
Joze Jenkole <joze.jenkole@gmail.com>
2020-12-23 00:43:56 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Jenx.CsCodeGenerators 
+
+More info: [https://www.jenx.si/2020/12/22/c-source-generators-a-very-useful-new-feature-of-c-compiler/](https://www.jenx.si/2020/12/22/c-source-generators-a-very-useful-new-feature-of-c-compiler/ "https://www.jenx.si/2020/12/22/c-source-generators-a-very-useful-new-feature-of-c-compiler/") 
---
+++ src/Consumer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Jenx.CsSourceCodeGeneratorConsumer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            CompileTimeGenerated.JenxWelcome.SayHello();
+            Console.ReadLine();
+
+            // executing my second C# Source Generator...
+            CompileTimeGenerated.PersonsWelcome.SayHelloFromAll();
+            Console.ReadLine();
+        }
+    }
+}
